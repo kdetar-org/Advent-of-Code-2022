fa0cd380349b9502b7105f059ee05d2acb8b5b7e
Kyle DeTar <kdetar@amazon.com>
2022-12-08 12:26:01 -0800

    solved day 7

--- AdventOfCode/src/App.java
+++ AdventOfCode/src/App.java
@@ -5,6 +5,9 @@ import Year_2022.Day3.*;
 import Year_2022.Day4.*;
 import Year_2022.Day5.*;
 import Year_2022.Day6.*;
+import Year_2022.Day7.*;
+import Year_2022.Day8.*;
+
 
 
 public class App {
@@ -16,27 +19,38 @@ public class App {
     private static final String DAY5_INPUT = "resources/Day5_Crate_Stacks.txt";
     private static final String DAY6_INPUT = "resources/Day6_Elf_Signal.txt";
     private static final String DAY6_FISHY_INPUT = "resources/Day6_2021_LanternFish.txt";
+    private static final String DAY7_INPUT = "resources/Day7_DirectoryInput.txt";
+    private static final String DAY8_INPUT = "resources/Day8_DirectoryInput.txt";
     
     public static void main(String[] args) throws Exception {
         
         System.out.println("\n -----Start of Block-----");
         new Day1_Part1(DAY1_INPUT).callWithMetrics("Day1 Part1: ");
         new Day1_Part2(DAY1_INPUT).callWithMetrics("Day1 Part2: ");
-        new Day2_Part1(DAY2_INPUT).callWithMetrics("\nDay2 Part1: ");
-        new Day2_Part2(DAY2_INPUT).callWithMetrics("Day2 Part2: ");
-        new Day3_Part1(DAY3_INPUT).callWithMetrics("\nDay3 Part1: ");
-        new Day3_Part2(DAY3_INPUT).callWithMetrics("Day3 Part2: ");
-        new Day4_Part1(DAY4_INPUT).callWithMetrics("\nDay4 Part1: ");
-        new Day4_Part2(DAY4_INPUT).callWithMetrics("Day4 Part2: ");
-        new Day5_Part1(DAY5_INPUT).callWithMetrics("\nDay5 Part1: ");
-        new Day5_Part2(DAY5_INPUT).callWithMetrics("Day5 Part2: ");
-        new Day6_Part1(DAY6_INPUT).callWithMetrics("\nDay6 Part1: ");
-        new Day6_Part2(DAY6_INPUT).callWithMetrics("Day6 Part2: ");
         
 
-        //Last Year's Advent
+        new Day2_Part1(DAY2_INPUT).callWithMetrics("\nDay2 Part1: ");
+        new Day2_Part2(DAY2_INPUT).callWithMetrics("Day2 Part2: ");
+        
+        new Day3_Part1(DAY3_INPUT).callWithMetrics("\nDay3 Part1: ");
+        new Day3_Part2(DAY3_INPUT).callWithMetrics("Day3 Part2: ");
+        
+        new Day4_Part1(DAY4_INPUT).callWithMetrics("\nDay4 Part1: ");
+        new Day4_Part2(DAY4_INPUT).callWithMetrics("Day4 Part2: ");
+        
+        new Day5_Part1(DAY5_INPUT).callWithMetrics("\nDay5 Part1: ");
+        new Day5_Part2(DAY5_INPUT).callWithMetrics("Day5 Part2: ");
+        
+        new Day6_Part1(DAY6_INPUT).callWithMetrics("\nDay6 Part1: ");
+        new Day6_Part2(DAY6_INPUT).callWithMetrics("Day6 Part2: ");
+        //Last Year's Day6
         new Day6(DAY6_FISHY_INPUT).callWithMetrics("\nDay6 2021: ");
 
+        new Day7_Part1(DAY7_INPUT).callWithMetrics("\nDay7 Part1: ");
+        new Day7_Part2(DAY7_INPUT).callWithMetrics("Day7 Part2: ");
+
+        new Day8_Part1(DAY8_INPUT).callWithMetrics("\nDay7 Part1: ");
+        new Day8_Part2(DAY8_INPUT).callWithMetrics("Day7 Part2: ");
                 
         System.out.println("\n -----End of Block-----\n"); 
     }
---
+++ AdventOfCode/src/Year_2022/Day7/ElfNode.java
@@ -0,0 +1,72 @@
+package Year_2022.Day7;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ElfNode implements Comparable<ElfNode>{
+    List<String> children;
+    int size;
+    String path;
+    Map<String,ElfNode> directoryMap;
+
+    public ElfNode(int size, String path) {
+        this.size = size;
+        this.path = path;
+        children = new ArrayList<String>();
+    }
+
+    public ElfNode(int size, String path, List<String> commands, Map<String,ElfNode> directoryMap){
+        this.size = size;
+        this.path = path;
+        this.directoryMap = directoryMap;
+        children = new ArrayList<String>();
+
+        //First command for this directory
+        String command = commands.remove(0);
+        
+        //The order of calls in a directory is CD dir (done) LS and CD..
+        if(command.equals("$ ls")){
+            command = commands.remove(0);
+            while(commands.size() >= 0 && command.charAt(0) != '$'){
+                String[] file = command.split(" ");
+
+                //if this is a directory
+                int memSize = file[0].equals("dir") ? 0 : Integer.parseInt(file[0]);
+                String str = file[0].equals("dir") ? path + file[1]+"/" : path + file[1];
+                ElfNode child = new ElfNode(memSize, str); 
+                children.add(child.path);
+                directoryMap.put(child.path, child);
+                command = commands.isEmpty() ? "$" : commands.remove(0);
+            }
+        }
+
+
+        // for cd.. we just fall off the end of the method
+        while(command != null && !commands.isEmpty()){
+            if(command.equals("$ cd ..")){
+                return;
+            }
+            else if(command.startsWith("$ cd")){      
+                String[] cmdSplit = command.split(" ");
+                directoryMap.put(path+cmdSplit[2]+"/", new ElfNode(0, path+cmdSplit[2]+"/", commands, directoryMap));
+                command = commands.isEmpty() ? null : commands.remove(0);
+            }
+        }
+    }
+
+    public int getSize() {
+        if (size == 0) {
+            for (String str : children) {
+                size += directoryMap.get(str).getSize();
+            }
+        }
+        return size;
+    }
+
+    @Override
+    public int compareTo(ElfNode o) {
+        return size - o.size;
+    }
+
+}
